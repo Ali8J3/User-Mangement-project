d5176a61fd5ffee2867838897487207aa33a08c1
Ali8J3 <alijoodakio6@gmail.com>
2023-09-19 20:49:51 +0330

    part#10

--- js/usersControler.js
+++ js/usersControler.js
@@ -1,9 +1,13 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("usersData", () => ({
     users: [],
+    pageUsers: [],
 
     isLoading: false,
     showAddModal: false,
+    pageCounts: 1,
+    itemsCount: 4,
+    currentPage: 1,
 
     getUsers() {
       this.isLoading = true;
@@ -12,10 +16,39 @@ document.addEventListener("alpine:init", () => {
         .get("https://jsonplaceholder.typicode.com/users")
         .then((res) => {
           this.users = res.data;
+          this.pagination();
         })
         .finally(() => {
           this.isLoading = false;
         });
+    },
+
+    pagination() {
+      this.pageCounts = Math.ceil(this.users.length / this.itemsCount); // pageCounts = number of total pages //users.length = number of total items(users)
+      let start = this.currentPage * this.itemsCount - this.itemsCount; // itemCounts = number of users that have been shown on pages(4=>8=>12=>...)
+      let end = this.currentPage * this.itemsCount;
+      this.pageUsers = this.users.slice(start, end);
+    },
+
+    nextPage() {
+      this.currentPage++;
+      this.pagination();
+      if (this.currentPage > this.pageCounts) {
+        this.currentPage = this.pageCounts;
+        this.pagination();
+      }
+    },
+    prevPage() {
+      this.currentPage--;
+      this.pagination();
+      if (this.currentPage < 1) {
+        this.currentPage = 1;
+        this.pagination();
+      }
+    },
+    handleChangeItemsCount(value) {
+      if (value < 1) this.itemsCount = 1;
+      if (value > this.users.length) this.itemsCount = this.users.length;
     },
   }));
 });
